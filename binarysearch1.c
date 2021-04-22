2bbe2989426cf38af953d03db08572ee07de2580
A-GHOSH-dev <75447439+A-GHOSH-dev@users.noreply.github.com>
2021-04-22 18:17:00 +0530

    Add files via upload

--- binarysearch1.c
+++ binarysearch1.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
+//Ananya Ghosh 20MIC0063
 int main()
 {
   int c, first, last, middle, n, search, array[100];
-
   printf("Enter number of elements\n");
   scanf("%d", &n);
-
   printf("Enter %d integers\n", n);
-
   for (c = 0; c < n; c++)
     scanf("%d", &array[c]);
-
   printf("Enter value to find\n");
   scanf("%d", &search);
-
   first = 0;
   last = n - 1;
   middle = (first+last)/2;
-
   while (first <= last) {
     if (array[middle] < search)
       first = middle + 1;
@@ -27,11 +22,9 @@ int main()
     }
     else
       last = middle - 1;
-
     middle = (first + last)/2;
   }
   if (first > last)
     printf("%d not present in the list.\n", search);
-
   return 0;
 }
